2a923392c33a2f8f3528bbd1b40ee428c2ea9268
Jesus <Jesus@DESKTOP-DCJT21S>
2019-05-26 09:34:42 +0200

    Versión 1.0 -- Sólo dice el nombre.

---
+++ ED_Git/src/ed_ct2/nom_Array.java
@@ -0,0 +1,10 @@
+package ed_ct2;
+public class nom_Array {
+	String[] Nombre = {"Harry", "Emilia", "Yolanda", "Amanda"};
+	String[] Apellidos = {"Price", "Clarke", "Collins", "Barber"};
+	public void mostrarNombreC() {
+		for (int i = 0; i<4; i++) {
+			System.out.println("El nombre del usuario " + i + " es: " + Nombre[i]);
+		}
+	}
+}
---
+++ ED_Git/src/ed_ct2/principal.java
@@ -0,0 +1,10 @@
+package ed_ct2;
+public class principal {
+
+	public static void main(String[] args) {
+		nom_Array n = new nom_Array();
+		n.mostrarNombreC();
+
+	}
+
+}
