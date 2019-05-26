7c5e12b9688a769152e46b39c07ebba09177462e
Jesus <Jesus@DESKTOP-DCJT21S>
2019-05-26 09:50:02 +0200

    Versión 3.0 --- Muestra Nombre y Apellido sin número.

--- ED_Git/src/ed_ct2/nom_Array.java
+++ ED_Git/src/ed_ct2/nom_Array.java
@@ -4,7 +4,8 @@ public class nom_Array {
 	String[] Apellidos = {"Price", "Clarke", "Collins", "Barber"};
 	public void mostrarNombreC() {
 		for (int i = 0; i<4; i++) {
-			System.out.println("El nombre del usuario " + i + " es: " + Nombre[i]);
+			System.out.println("El nombre del usuario es: " + Nombre[i] + ".\n"
+					+ "Y el apellido de dicho usuario es: " + Apellidos[i]);
 		}
 	}
 }
